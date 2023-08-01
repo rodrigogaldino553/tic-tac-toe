06626a0e031f514b3614f46191cdf45150439df2
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-08-01 00:10:23 -0300

    add rules to define a tie

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,23 +3,53 @@
 // a cada rodada rodar pra ver se o jogo chegou em alguma combinação de vitoria
 
 var x = 0
+const plays = ['O', 'X']
+
+var buttons_values = {btn_1: '', btn_2: '', btn_3: '',
+                      btn_4: '', btn_5: '', btn_6: '',
+                      btn_7: '', btn_8: '', btn_9: ''}
 
 
 function btn_play(event){
   let current_btn = event.target
-  let current_btn_value = current_btn.value
 
-  if(current_btn_value !== '') return
-  
-  let play = turn()
-  current_btn.innerHTML = play
-  current_btn.value = play
+  if(current_btn.value !== '') return
+
+  set_button_value(current_btn, turn())
+  end_game()
 }
 
 function turn(){
-  let plays = ['O', 'X']
   x = Number(!x)
   return plays[x]
+}
+
+function set_button_value(button, turn_value){
+  button.innerHTML = turn_value
+  button.value = turn_value
+
+  buttons_values[button.id] = turn_value
+}
+
+// Rules to determine the game result
+
+function end_game(){
+  if(atie()){
+    alert('A TIE!')
+  }else{
+    return false
+  }
+
+  // ações quando o jogo termina
+  // zerar variaveis, e dar opção de jogar de novo
+}
+
+
+function atie(){
+  for(var btn_id in buttons_values){
+    if(buttons_values[btn_id] === '') return false
+  }
+  return true
 }
 
 
